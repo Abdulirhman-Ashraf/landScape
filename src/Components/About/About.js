19aca924559c160d449e15391c45c93a6f5d9eeb
Abdulirhman-Ashraf <153239328+Abdulirhman-Ashraf@users.noreply.github.com>
2024-10-11 21:50:39 +0300

    Update About.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,7 +5,7 @@ const About = (props) => {
     <div className="about  container w-75" id={props.id}>
       <div className="row d-flex align-items-center">
         <div className="col col-md-6 col-sm-12">
-          <img src={props.img} />
+          <img src={props.img} alt="image"/>
         </div>
         <div className="col col-md-6 col-sm-12">
           <h2 className="w-50">{props.h2}</h2>
